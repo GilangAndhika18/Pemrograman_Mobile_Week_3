09895288a156eaa02ac0994fd251b3991c9777ff
Gilang Andhika Erlangga <gilangandhikaerlangga18@gmail.com>
2025-09-22 20:35:30 +0700

    commit bmi-calculation

--- dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
+++ dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
@@ -3,3 +3,27 @@ import 'package:dart_kalkulator_bmi/dart_kalkulator_bmi.dart' as dart_kalkulator
 void main() { 
   List<Map<String, dynamic>> riwayat = [];
 }
+
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) {
+  double tinggiM = tinggiCm / 100;
+  double bmi = beratKg / (tinggiM * tinggiM);
+
+  String kategori;
+  if (bmi < 18.5) {
+    kategori = "Kurus";
+  } else if (bmi < 25) {
+    kategori = "Normal";
+  } else if (bmi < 30) {
+    kategori = "Gemuk";
+  } else {
+    kategori = "Obesitas";
+  }
+
+  // Simpan hasil ke riwayat
+  riwayat.add({
+    'tinggi': tinggiCm,
+    'berat': beratKg,
+    'bmi': bmi,
+    'kategori': kategori,
+  });
+}
