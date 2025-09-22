7201aee54e6ca280bb5308ca5f0905ad845cdd8c
Gilang Andhika Erlangga <gilangandhikaerlangga18@gmail.com>
2025-09-22 20:12:53 +0700

    another commit

--- dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
+++ dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
@@ -1,10 +1,5 @@
 import 'package:dart_kalkulator_bmi/dart_kalkulator_bmi.dart' as dart_kalkulator_bmi;
 
-// void main(List<String> arguments) {
-//   print('Hello world: ${dart_kalkulator_bmi.calculate()}!');
-// }
-
-// main.dart
 void main() { 
   List<Map<String, dynamic>> riwayat = [];
 }
