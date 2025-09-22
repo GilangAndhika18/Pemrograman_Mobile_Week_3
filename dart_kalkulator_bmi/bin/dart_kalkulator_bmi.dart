b06fea3b5e9dd705eff56c8674cff9a00226f4da
Gilang Andhika Erlangga <gilangandhikaerlangga18@gmail.com>
2025-09-22 20:38:01 +0700

    commit history-feature

--- dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
+++ dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
@@ -3,3 +3,12 @@ import 'package:dart_kalkulator_bmi/dart_kalkulator_bmi.dart' as dart_kalkulator
 void main() { 
   List<Map<String, dynamic>> riwayat = [];
 }
+
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  for (var data in riwayat) {
+    print("Tinggi: ${data['tinggi']} cm, "
+        "Berat: ${data['berat']} kg, "
+        "BMI: ${data['bmi'].toStringAsFixed(2)}, "
+        "Kategori: ${data['kategori']}");
+  }
+}
