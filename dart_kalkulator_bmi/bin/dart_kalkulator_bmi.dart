da420947233ddf2d46a91d6b84fcc2992e15dd14
Gilang Andhika Erlangga <gilangandhikaerlangga18@gmail.com>
2025-09-22 19:22:50 +0700

    revisi main

--- dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
+++ dart_kalkulator_bmi/bin/dart_kalkulator_bmi.dart
@@ -5,14 +5,6 @@ import 'package:dart_kalkulator_bmi/dart_kalkulator_bmi.dart' as dart_kalkulator
 // }
 
 // main.dart
-void main() {
+void main() { 
   List<Map<String, dynamic>> riwayat = [];
-
-  // Simulasi beberapa input
-  hitungBMI(170, 65, riwayat);
-  hitungBMI(165, 75, riwayat);
-  hitungBMI(180, 85, riwayat);
-
-  // Tampilkan riwayat menggunakan perulangan
-  tampilkanRiwayat(riwayat);
 }
